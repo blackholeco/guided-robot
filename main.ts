48134b8d7e2946d9fba1305bda03901b8460b4f3
blackholeco <35765087+blackholeco@users.noreply.github.com>
2022-03-18 21:58:47 +0000

    Added new go function to simplify robot movement control.

--- main.ts
+++ main.ts
@@ -62,30 +62,52 @@ function right_lights (lights_on: boolean) {
         led.unplot(4, 1)
     }
 }
+function go (direction: string, milliseconds: number) {
+    if (direction == "forward") {
+        left_wheel(1)
+        right_wheel(1)
+    } else if (direction == "left") {
+        left_wheel(0)
+        right_wheel(1)
+    } else if (direction == "right") {
+        left_wheel(1)
+        right_wheel(0)
+    } else if (direction == "reverse") {
+        left_wheel(-1)
+        right_wheel(-1)
+    }
+    if (milliseconds > 0) {
+        basic.pause(milliseconds)
+    }
+}
 let running = false
 let reverse = 0
 let forward = 0
 let maxSpeed = 0
+let stop = 0
 maxSpeed = 1000
 forward = 1
 reverse = -1
-let stop = 0
 running = false
-/**
- * Sonar-Guided Robot notes:
- * 
- * Each wheel can be controlled independently using the left_wheel() and right_wheel functions.  Pass in any of forward, reverse or stop to control which direction it travels.
- * 
- * maxSpeed controls the speed at which the wheels turn.  1000 should be the maximum, but this could be reduced depending on the surface the robot is running on.
- * 
- * Complete the forever block to get the robot moving through the maze.  This should constantly fire a sonar pulse ahead of the robot, sense the returning pulse, and then give a distance to the next wall.
- * 
- * Use the Sonar extension (should be included) to use a sensor to detect a distance to an object in front of the robot.  If the distance is small enough, make a right turn and try to move forward from there.  If we get too close to a wall, try to reverse a short distance.
- * 
- * Maze solving is based on the principle of the Right-Hand rule, where a simple maze can be solved by following the wall on your right.  While you may hit a number of dead-ends, you should eventually reach the exit.
- */
+// Sonar-Guided Robot notes:
+// 
+// Each wheel can be controlled independently using the left_wheel() and right_wheel functions.  Pass in any of forward, reverse or stop to control which direction it travels.
+// 
+// maxSpeed controls the speed at which the wheels turn.  1000 should be the maximum, but this could be reduced depending on the surface the robot is running on.
+// 
+// Complete the forever block to get the robot moving through the maze.  This should constantly fire a sonar pulse ahead of the robot, sense the returning pulse, and then give a distance to the next wall.
+// 
+// Use the Sonar extension (should be included) to use a sensor to detect a distance to an object in front of the robot.  If the distance is small enough, make a right turn and try to move forward from there.  If we get too close to a wall, try to reverse a short distance.
+// 
+// Maze solving is based on the principle of the Right-Hand rule, where a simple maze can be solved by following the wall on your right.  While you may hit a number of dead-ends, you should eventually reach the exit.
 basic.forever(function () {
     if (running == true) {
-    	
+        for (let index = 0; index <= 3; index++) {
+            go("forward", 1000)
+            brake()
+            go("right", 1000)
+            brake()
+        }
+        running = false
     }
 })
